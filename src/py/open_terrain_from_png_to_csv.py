16ea471154717e9258569b42c423de3f3b8784d2
G-Chist <42654606+G-Chist@users.noreply.github.com>
2025-08-01 13:27:54 -0400

    testing if irl terrain was imported properly

--- src/py/open_terrain_from_png_to_csv.py
+++ src/py/open_terrain_from_png_to_csv.py
@@ -5,12 +5,35 @@ import numpy as np
 import bpy
 import os
 
-from utils import write_vertices_to_csv, load_bw_image_as_normalized_array, grid_to_xyz, apply_convolution, box_blur_25x25, box_blur_11x11
+from utils import write_vertices_to_csv, load_bw_image_as_normalized_array, grid_to_xyz, apply_convolution, box_blur_25x25, box_blur_11x11, feature_map
 
 
 if __name__ == "__main__":
     terrain = load_bw_image_as_normalized_array("C:\\Users\\79140\\PycharmProjects\\procedural-terrain-generation-blender\\data\\terrain_example.png")
     terrain = apply_convolution(matrix=terrain, kernel=box_blur_11x11)  # smoothen
+
+    """
+    # TESTING
+    
+    import matplotlib.pyplot as plt
+
+    fig, axs = plt.subplots(1, 2, figsize=(12, 10))
+
+    axs[0].imshow(terrain, cmap='gray', interpolation='lanczos')
+    axs[0].set_title("Real Terrain")
+    axs[0].axis('off')
+    fig.colorbar(axs[0].images[0], ax=axs[0])
+
+    axs[1].imshow(feature_map(terrain), cmap='gray', interpolation='lanczos')
+    axs[1].set_title("Real Terrain Feature Map")
+    axs[1].axis('off')
+    fig.colorbar(axs[1].images[0], ax=axs[1])
+
+    plt.tight_layout()
+    plt.show()
+    
+    """
+
     vertices = grid_to_xyz(terrain, start_coordinate=-6, end_coordinate=6).tolist()
     size = terrain.shape[0]
 
