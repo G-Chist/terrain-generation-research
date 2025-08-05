1cc8739fc43b3226421bfc60c05390bee6e77455
G-Chist <42654606+G-Chist@users.noreply.github.com>
2025-08-05 13:34:14 -0400

    3d plot changed to 2d plot

--- src/py/open_and_plot_tif.py
+++ src/py/open_and_plot_tif.py
@@ -6,21 +6,9 @@ from utils import load_elevation_grid
 
 if __name__ == "__main__":
 
-    elevation, x_coords, y_coords = load_elevation_grid(r"C:\Users\79140\Downloads\USGS_1M_17_x54y411_VA_FEMA-NRCS_SouthCentral_2017_D17.tif")
+    elevation, x_coords, y_coords = load_elevation_grid(r"C:\Users\79140\PycharmProjects\procedural-terrain-generation\data\USGS_1M_17_x54y411_VA_FEMA-NRCS_SouthCentral_2017_D17.tif")
 
-    # Plot in 3D
-    fig = plt.figure(figsize=(12, 8))
-    ax = fig.add_subplot(111, projection='3d')
-
-    ax.plot_surface(
-        x_coords,
-        y_coords,
-        elevation,
-        cmap='terrain', linewidth=0, antialiased=False
-    )
-
-    ax.set_xlabel('X coordinate')
-    ax.set_ylabel('Y coordinate')
-    ax.set_zlabel('Elevation')
-
+    # Plot in 2D
+    plt.imshow(elevation, cmap='gray', interpolation='lanczos')  # height map
+    plt.colorbar()
     plt.show()
