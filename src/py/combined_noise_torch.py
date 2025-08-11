56a0ae330984f831fefef915c1cf71428dba06a5
G-Chist <42654606+G-Chist@users.noreply.github.com>
2025-08-11 12:10:37 -0400

    playing around with params

--- src/py/combined_noise_torch.py
+++ src/py/combined_noise_torch.py
@@ -204,9 +204,9 @@ if __name__ == '__main__':
 
     # === Parameters ===
     res = 2000
-    perlin_res = (8, 8)
+    perlin_res = (20, 20)
     scale_wm = 50
-    alpha = 0.6  # blending factor, describes the magnitude of Perlin Noise (can be negative)
+    alpha = 0.3  # blending factor, describes the magnitude of Perlin Noise (can be negative)
     seed = 1738
 
     gen = torch.Generator(device='cuda' if torch.cuda.is_available() else 'cpu').manual_seed(seed)  # seed random
