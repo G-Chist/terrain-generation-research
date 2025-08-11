353ddb11f6ad23b957afa3b4d04f536c8ab76f12
G-Chist <42654606+G-Chist@users.noreply.github.com>
2025-08-11 16:47:23 -0400

    better params

--- src/py/combined_noise_torch.py
+++ src/py/combined_noise_torch.py
@@ -207,10 +207,10 @@ if __name__ == '__main__':
     # === Parameters ===
     res = 2000
     perlin_res = (5, 5)
-    scale_wm = 2000
-    alpha = 0.2  # blending factor, describes the magnitude of Perlin Noise
+    scale_wm = 4000
+    alpha = 0.3  # blending factor, describes the magnitude of Perlin Noise
     seed = 1738
-    wm_mag = 0.3
+    wm_mag = 0.1
 
     gen = torch.Generator(device='cuda' if torch.cuda.is_available() else 'cpu').manual_seed(seed)  # seed random
 
@@ -223,10 +223,10 @@ if __name__ == '__main__':
 
     # === WM Layers ===
     wm_layers = [
-        {'D': 2.0, 'G': 1e-7, 'L': 1000.0, 'gamma': 1.5, 'M': 8, 'n_max': 10},
-        {'D': 2.55, 'G': 8e-8, 'L': 1000.0, 'gamma': 1.5, 'M': 32, 'n_max': 10},
-        {'D': 2.45, 'G': 1e-8, 'L': 1000.0, 'gamma': 1.5, 'M': 64, 'n_max': 10},
-        {'D': 2.45, 'G': 1e-8, 'L': 1000.0, 'gamma': 1.5, 'M': 256, 'n_max': 10},
+        {'D': 2.0, 'G': 1e-7, 'L': 100.0, 'gamma': 1.5, 'M': 8, 'n_max': 10},
+        {'D': 2.55, 'G': 8e-8, 'L': 80.0, 'gamma': 3.5, 'M': 32, 'n_max': 10},
+        {'D': 2.45, 'G': 1e-8, 'L': 55.0, 'gamma': 2.5, 'M': 64, 'n_max': 10},
+        {'D': 3.25, 'G': 1e-8, 'L': 40.0, 'gamma': 8.5, 'M': 256, 'n_max': 10},
     ]
 
     noise = generate_combined_noise(
